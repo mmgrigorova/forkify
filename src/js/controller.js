3aa52e6ee83b21e9f7413c34e13060c41add41fe
Maria Grigorova <mmgrigorova@gmail.com>
2022-04-21 18:50:46 +0300

    Search by query

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,7 +3,12 @@ import 'regenerator-runtime/runtime';
 
 import * as model from './model.js'
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
+if (module.hot) {
+  module.hot.accept();
+}
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
@@ -26,8 +31,28 @@ const controlRecipes = async function () {
   }
 };
 
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+
+    // Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // Load search results
+    await model.loadSearchResults(query);
+
+    // Render results
+    resultsView.render(model.state.search.results);
+  }
+  catch (err) {
+    console.log(err);
+  }
+}
+
 // publisher-subscriber pattern
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 }
 init();
